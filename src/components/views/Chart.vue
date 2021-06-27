2c62cf1bbfdfd5d3887beeedb8d875f61a12a657
Luis Luna <luismluna87@gmail.com>
2021-06-26 23:38:11 -0300

    Added support for data requests and inputs for basic settings

--- src/components/views/Chart.vue
+++ src/components/views/Chart.vue
@@ -1,7 +1,12 @@
 <template>
-  <button v-on:click="addRandomData">Agregar dato de test</button>
+  <div id="input-bar">
+    <label>Intervalo de actualización: </label>
+    <input v-model.number="updateInterval" type="number" />
+    <label>Cant. muestras: </label>
+    <input v-model.number="sampleCount" type="number" />
+  </div>
   <div class="chart-wrapper">
-  <canvas :id="CHART_DOM_ID"></canvas>
+    <canvas :id="CHART_DOM_ID"></canvas>
   </div>
 </template>
 
@@ -9,8 +14,23 @@
 import moment from "moment";
 import _ from "lodash";
 import Chart from "chart.js/auto";
+import axios from "axios";
+import { ref } from "vue";
 
-const CHART_DOM_ID = "data-container";
+const CONSTANTS = {
+  CHART_DOM_ID: "data-container",
+  DATA_URL: "http://localhost:3000/fakeEEG/single",
+  MAX_SAMPLE_COUNT: 30,
+  UPDATE_INTERVAL: 5000,
+};
+
+/* //////////////////////////////////////////////////// */
+/* BINDED VALUEs */
+let updateInterval = ref(CONSTANTS.UPDATE_INTERVAL);
+let sampleCount = ref(CONSTANTS.MAX_SAMPLE_COUNT);
+
+/* //////////////////////////////////////////////////// */
+/* SUPPORT CLASSES */
 
 const DataSet = function (name, rgbColor) {
   name = name || "Indefinido";
@@ -19,34 +39,95 @@ const DataSet = function (name, rgbColor) {
   this.backgroundColor = "rgba(" + rgbColor + ", 0.2)";
   this.borderColor = "rgba(" + rgbColor + ", 1)";
   this.borderWidth = 1;
-  this.fill = 'origin';
+  this.fill = "origin";
 };
 
-let DataSetData = function (dataSetName, data) {
+const DataSetData = function (dataSetName, data) {
   this.dataSetName = dataSetName;
   this.data = data;
 };
 
-let DataSetItem = function (ts, value) {
+const DataSetItem = function (ts, value) {
   this.ts = ts;
   this.value = value;
 };
 
-function updateChart(data) {
-  const chart = Chart.getChart(CHART_DOM_ID);
+/* //////////////////////////////////////////////////// */
 
-  let dataSet = null;
+/* METHODS */
+
+function getChart() {
+  return Chart.getChart(CONSTANTS.CHART_DOM_ID);
+}
+
+function getChartTimeStamps(chart) {
+  if (typeof chart == "undefined" || chart == null) chart = getChart();
+
+  return _.chain(chart.data.datasets)
+    .map((dataSet) => _.map(dataSet.data))
+    .flatten()
+    .map((data) => data.x)
+    .orderBy((ts) => ts)
+    .sortedUniq()
+    .value();
+}
+
+function fixXValueCount(offset = 1) {
+  let timeStamps = getChartTimeStamps();
+  let maxSampleCount = sampleCount.value;
+  if (timeStamps.length < maxSampleCount) return;
+
+  let chart = getChart();
+
+  /* Removes only 1, it should remove all the extra samples */
+  let countToRemove = timeStamps.length - maxSampleCount + offset;
+  if (countToRemove <= 0) return;
+
+  let currentDataSet = null;
+  let currentTs = null;
+  let tsToRemove = _.slice(timeStamps, 0, countToRemove);
+
+  for (let i = 0; i < tsToRemove.length; i++) {
+    currentTs = tsToRemove[i];
+    for (let j = 0; j < chart.data.datasets.length; j++) {
+      currentDataSet = chart.data.datasets[j];
+      for (let k = 0; k < currentDataSet.data.length; k++) {
+        if (currentDataSet.data[k].x == currentTs) {
+          currentDataSet.data.shift();
+          break;
+        }
+      }
+    }
+  }
+}
+
+/* Receives a data collection and ads it to the chart */
+function updateChart(data) {
+  /* Gets chart by dom id */
+  const chart = getChart();
+
+  let chartNewData = null;
   let dataSetIndex = null;
   let dataItem = null;
+
+  /* Each data object has a DataSet name, used to finde the data set in the chart settings */
   for (let i = 0; i < data.length; i++) {
-    dataSet = data[i];
+    chartNewData = data[i];
     dataSetIndex = _.findIndex(
       chart.data.datasets,
-      (o) => o.label == dataSet.dataSetName
+      (chartDataSet) => chartDataSet.label == chartNewData.dataSetName
     );
 
-    for (let j = 0; j < dataSet.data.length; j++) {
-      dataItem = dataSet.data[j];
+    if (dataSetIndex < 0) {
+      console.error("DataSet not found cant update chart");
+    }
+
+    /* Fix the ammount of points in chart */
+    fixXValueCount();
+
+    /* Each dataSet has a collection of data items that has to be added to the dataSet data items */
+    for (let j = 0; j < chartNewData.data.length; j++) {
+      dataItem = chartNewData.data[j];
       chart.data.datasets[dataSetIndex].data.push({
         x: dataItem.ts,
         y: dataItem.value,
@@ -54,12 +135,11 @@ function updateChart(data) {
     }
   }
 
-  let timeStamps = _.chain(chart.data.datasets)
-    .map((dataSet) => _.map(dataSet.data))
-    .flatten()
-    .map((data) => data.x)
-    .orderBy((ts) => ts)
-    .sortedUniq()
+  /* 
+  Searches the timestamps on all of the datasets (x axis value) 
+  Orders them and creates an automatic labels collection
+  */
+  let timeStamps = _.chain(getChartTimeStamps())
     .map((ts) => moment(ts).format("DD/MM/YY HH:mm:ss"))
     .value();
 
@@ -69,7 +149,7 @@ function updateChart(data) {
 }
 
 function setChart(dataSets) {
-  let ctx = document.getElementById(CHART_DOM_ID).getContext("2d");
+  let ctx = document.getElementById(CONSTANTS.CHART_DOM_ID).getContext("2d");
 
   let chartSettings = {
     type: "line",
@@ -78,7 +158,7 @@ function setChart(dataSets) {
       datasets: dataSets,
     },
     options: {
-        maintainAspectRatio: false,
+      maintainAspectRatio: false,
       scales: {
         yAxes: [
           {
@@ -96,8 +176,6 @@ function addRandomData() {
   const max = 15000;
   const min = 5000;
 
-  console.clear();
-
   let data = [
     new DataSetData("Alpha", [
       new DataSetItem(moment()._d, Math.random() * (max - min) + min),
@@ -107,14 +185,52 @@ function addRandomData() {
   updateChart(data);
 }
 
+function getData() {
+  return axios.get(CONSTANTS.DATA_URL);
+}
+
+function processChartRequest(response) {
+  updateChart([
+    new DataSetData("Alpha", [
+      new DataSetItem(
+        moment(response.data.ts)._d,
+        response.data.eegPower.highAlpha
+      ),
+    ]),
+  ]);
+}
+
+let createRequest;
+createRequest = function () {
+  getData().then((response) => {
+    processChartRequest(response);
+    setTimeout(createRequest, updateInterval.value);
+  });
+};
+
+function startDataRequest() {
+  /* First request is manual and immediate, later will be automatic by interval */
+  getData().then(processChartRequest);
+  setInterval(() => {
+    getData().then(processChartRequest);
+  }, updateInterval.value);
+}
+
+/* //////////////////////////////////////////////////// */
+
+/* COMPONENT DEFINITION */
+
 export default {
   mounted() {
     setChart([new DataSet("Alpha", "0, 0, 255")]);
+    createRequest();
   },
   setup() {
     return {
-      CHART_DOM_ID,
+      CHART_DOM_ID: CONSTANTS.CHART_DOM_ID,
       addRandomData,
+      updateInterval: updateInterval,
+      sampleCount,
     };
   },
 };
@@ -122,8 +238,16 @@ export default {
 
 <style>
 .chart-wrapper {
-    height: calc(100vh - 90px);
-    position: relative;
-    overflow: hidden;
+  height: calc(100vh - 90px);
+  position: relative;
+  overflow: hidden;
+}
+#input-bar {
+  padding-top: 5px;
+}
+
+#input-bar > * {
+  margin-right: 1vw;
+    width: 8vw;
 }
 </style>
