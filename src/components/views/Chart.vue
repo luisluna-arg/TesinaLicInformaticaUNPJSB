d66da38bfae84291bc47eaaaaa0c9b336aae336d
Luis Luna <luismluna87@gmail.com>
2022-03-20 22:16:28 -0300

    Added JSON file upload for static data

--- src/components/views/Chart.vue
+++ src/components/views/Chart.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
-    <div id="input-bar">
-      <label>Intervalo de actualización (ms): </label>
+    <div id="input-bar" class="input-bar">
+      <label>Lectura vivo: </label>
+      <input type="checkbox" id="liveChart" v-model="liveChart" @change="onChange_FromDisk" >
+      <label>Interv. actualiz. (ms): </label>
       <input v-model.number="updateInterval" readonly type="number" />
-      <label>Cant. muestras: </label>
+      <label>Muestras: </label>
       <input v-model.number="sampleCount" @change="onChange_SampleCout" type="number" />
       <label>Atención: </label>
       <input v-model.number="attention" type="number" />
       <label>Meditación: </label>
       <input v-model.number="meditation" type="number" />
+    </div>
+    <div class="input-bar">
+      <label>Media: </label>
+      <input v-model.number="meanVal" type="number" />
+      <label>Varianza: </label>
+      <input v-model.number="varianceVal" type="number" />
+      <label>Dev. Std.: </label>
+      <input v-model.number="devStdVal" type="number" />
+      <label for="jsonFile">JSON: </label>
+      <input type="file" id="jsonFile" name="jsonFile" accept=".json" @change="onChange_JsonFile">
     </div>
     <div class="chart-wrapper">
       <canvas :id="CHART_DOM_ID"></canvas>
@@ -22,6 +34,7 @@ import _ from "lodash";
 import Chart from "chart.js/auto";
 import axios from "axios";
 import { ref } from "vue";
+import * as tf from "@tensorflow/tfjs";
 
 const CONSTANTS = {
   CHART_DOM_ID: "data-container",
@@ -42,6 +55,11 @@ const DATASET_NAMES = {
 let LIVE_VALUES = {
   ATTENTION: 0,
   MEDITATION: 0,
+  MEDIA: 0,
+  VARIANZA: 0,
+  DEV_STD: 0,
+  LIVE_CHART: false,
+  STATIC_DATA: [],
   DATA_URL: CONSTANTS.DATA_URL + "?size=" + CONSTANTS.MAX_SAMPLE_COUNT,
 };
 
@@ -52,8 +70,15 @@ let sampleCount = ref(CONSTANTS.MAX_SAMPLE_COUNT);
 
 let attention = ref(LIVE_VALUES.ATTENTION);
 let meditation = ref(LIVE_VALUES.MEDITATION);
+let liveChart = ref(LIVE_VALUES.LIVE_CHART);
 
 let dataUrl = ref(LIVE_VALUES.DATA_URL);
+
+let staticData = ref(LIVE_VALUES.STATIC_DATA);
+
+let meanVal = ref(LIVE_VALUES.MEDIA);
+let varianceVal = ref(LIVE_VALUES.VARIANZA);
+let devStdVal = ref(LIVE_VALUES.DEV_STD);
 
 /* //////////////////////////////////////////////////// */
 /* SUPPORT CLASSES */
@@ -84,6 +109,20 @@ const DataSetItem = function (ts, value) {
 
 function onChange_SampleCout() {
   dataUrl.value = CONSTANTS.DATA_URL + "?size=" + sampleCount.value;
+}
+
+function onChange_FromDisk(e){
+  console.log(e);
+} 
+
+function onFileReaderLoad(e) {
+  staticData.value = JSON.parse(e.target.result);
+}
+
+function onChange_JsonFile(e) {
+  var reader = new FileReader();
+  reader.onload = onFileReaderLoad;
+  reader.readAsText(e.srcElement.files[0]);
 }
 
 /* //////////////////////////////////////////////////// */
@@ -189,6 +228,10 @@ function addRandomData() {
 
 function processChartRequest(response) {
   if (!!response.data && response.data.length > 0) {
+    if (!liveChart.value) {
+      sampleCount.value = response.data.length;
+    }
+
     let lowAlpha = new DataSetData(DATASET_NAMES.LowAlpha);
     let highAlpha = new DataSetData(DATASET_NAMES.HighAlpha);
     let lowBeta = new DataSetData(DATASET_NAMES.LowBeta);
@@ -196,8 +239,18 @@ function processChartRequest(response) {
     let lowGamma = new DataSetData(DATASET_NAMES.LowGamma);
     let highGamma = new DataSetData(DATASET_NAMES.HighGamma);
 
+    let samples = [];
     for (let i = 0; i < response.data.length; i++) {
       let data = response.data[i];
+
+      samples.push([
+        data.eegPower.lowAlpha,
+        data.eegPower.highAlpha,
+        data.eegPower.lowBeta,
+        data.eegPower.highBeta,
+        data.eegPower.lowGamma,
+        data.eegPower.highGamma
+      ]);
 
       lowAlpha.data.push(new DataSetItem(moment(data.ts)._d, data.eegPower.lowAlpha));
       highAlpha.data.push(new DataSetItem(moment(data.ts)._d, data.eegPower.highAlpha));
@@ -214,15 +267,37 @@ function processChartRequest(response) {
       attention.value = latestData.eSense.attention;
       meditation.value = latestData.eSense.meditation;
     }
+
+    let meanTFVal = null;
+    let varianceTFVal = null;
+    let devStdTFVal = null;
+    tf.tidy(() => {
+      const { mean, variance } = tf.moments(samples);
+      const devStd = tf.sqrt(variance);
+
+      meanTFVal = mean.dataSync()[0];
+      varianceTFVal = variance.dataSync()[0];
+      devStdTFVal = devStd.dataSync()[0];
+    });
+
+    meanVal.value = parseFloat(meanTFVal.toFixed(2));
+    varianceVal.value = parseFloat(varianceTFVal.toFixed(2));
+    devStdVal.value = parseFloat(devStdTFVal.toFixed(2));
   }
 }
 
 let createRequest;
 createRequest = function () {
-  axios.get(dataUrl.value).then((response) => {
-    processChartRequest(response);
+  if (liveChart.value){
+    axios.get(dataUrl.value).then((response) => {
+      processChartRequest(response);
+      setTimeout(createRequest, updateInterval.value);
+    });
+  }
+  else {
+    processChartRequest({ data: staticData.value });
     setTimeout(createRequest, updateInterval.value);
-  });
+  }
 };
 
 /* //////////////////////////////////////////////////// */
@@ -249,6 +324,12 @@ export default {
       sampleCount,
       attention,
       meditation,
+      liveChart,
+      meanVal,
+      varianceVal,
+      devStdVal,
+      onChange_FromDisk,
+      onChange_JsonFile,
       onChange_SampleCout
     };
   },
@@ -256,6 +337,25 @@ export default {
 </script>
 
 <style>
+.input-bar {
+  margin-top: 5px;
+}
+
+.input-bar:first {
+  margin-top: 0;
+}
+
+input {
+  margin-right: 5px;
+}
+
+input:last-child {
+  margin-right: 0;
+}
+
+label {
+  font-weight: bold;
+}
 .chart-wrapper {
   height: calc(100vh - 90px);
   position: relative;
