8bbe12ba1011d2dc2decf556354eaabc757f704d
gdejesus <guille17@gmail.com>
2022-05-15 16:53:43 -0300

    upd firestore config

---
+++ node_server_engine/src/config/firebase.config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
